e7e7dadb311b0e87129062a53a4165c42063efa1
Aravinthan <aravinthanpro@gmail.com>
2021-05-15 19:25:41 +0530

    Arrow function and callback function

--- Functions/script.js
+++ Functions/script.js
@@ -43,3 +43,48 @@ console.log(getUserRole("aravinthan", "admin"));
 
 console.log(printUserRole("praba", "admin", "user"));
 
+
+
+
+//Next level functions
+
+//Normal function
+function isEven(number) {
+    return number % 2 === 0
+}
+// Var function 
+var varISEven = function(number) {
+    return number %2 === 0
+}
+
+console.log(isEven(3))
+console.log(varISEven(2))
+
+
+// Arrow function
+var arrowIsEven = (number) => {
+    return number %2 === 0
+}
+
+console.log(arrowIsEven(4))
+
+//CallBAck function - every method have call back function, 
+//We pass the function as reference , not call the function
+
+var result = [2,4,6,8].every(arrowIsEven)
+console.log("Result : "+result)
+
+
+// In 2020 try to write callback function in arrow function manner
+/*
+(e) => {
+    If {} is used need to return some thing
+}
+
+(e) => (
+    if ()  is no need to return  
+)
+*/
+
+var callbackResult = [2,4,6,7].every( (e) => (e % 2 === 0) )
+console.log("Call back result " +callbackResult)
